6f0404be11dee77b756b859670b5517cac509a3f
황신애 <shinae1023@naver.com>
2026-01-12 20:55:53 +0900

    11726 solve

--- 연습장.java
+++ 연습장.java
@@ -1,48 +1,30 @@
 import java.util.*;
 import java.io.*;
 
-<<<<<<< HEAD
 class Main {
     public static void main(String[] args) throws IOException {
         // 1. 입력을 위한 BufferedReader (Scanner보다 훨씬 빠름)
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 2. 출력을 한데 모으기 위한 StringBuilder (System.out.println 여러 번보다 빠름)
-        StringBuilder sb = new StringBuilder();
 
-        // 3. 테스트 케이스 수 입력 받기
+        // 2. n (가로 크기) 입력 받기
         int n = Integer.parseInt(br.readLine());
-        if (n==0) {System.out.println(0); return;}
 
-        for(int i = 0; i < n; i++){
-            // 4. 의상의 수 입력 받기 & 필요 변수 생성 및 초기화
-            int m = Integer.parseInt(br.readLine());
-            HashMap<String, Integer> map = new HashMap<>();
-            int result = 1;
+        // 3. 알고리즘 로직 작성
+        /** think : 1) 세로 블럭을 두는 경우 2*n-1 수행, 2) 가로 블럭을 두는 경우 2*n-2 수행
+         * n=1 1, n=2 2,
+         * n = 3 111 12 21 3 / n = 4 1111 112 121 211 22  5/
+         * n = 5 11111 1112 1121 1211 2111 221 121 122 8/ n = 6 111111 
+         * -> 피보나치
+          */
+        
+        int[] arr = new int[1001];
+        arr[1]=1; arr[2]  = 2;
+        
+        for(int i=3; i<=n; i++){
+            arr[i] = (arr[i-1] + arr[i-2])%10007;
+        }
 
-            for(int j=0; j<m; j++){
-                // 5. 알고리즘 작성
-                /**think : 
-                 *  HashMap<String, Integer>으로 저장
-                 * -> 정수형으로 종류 당 몇개 있는지 count
-                 * -> result = count+1들의 곱 */ 
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                String name = st.nextToken(); 
-                String type = st.nextToken();
-                map.put(type, map.getOrDefault(type, 0) + 1);
-            }
-
-            for (int count : map.values()) {
-                result *= (count + 1);
-            }
-
-            result -= 1;
-
-            // 6. 각 출력값 String Builder에 저장
-            sb.append(result).append("\n");
-        } 
-
-        // 7. 결과 출력
-        System.out.print(sb);
+        // 4. 결과 출력
+        System.out.print(arr[n]);
     }
 }
