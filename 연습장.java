9b97bb36f01bef69c0e9b2ec3a81de269111f0aa
황신애 <shinae1023@naver.com>
2026-01-20 16:33:20 +0900

    2805 디버깅용

--- 연습장.java
+++ 연습장.java
@@ -6,90 +6,61 @@ class Main {
         // 1. 입출력을 위한 BufferedReader, StringBuilder
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder();
-
         // 2. 필요한 변수 입력받기
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int t = Integer.parseInt(st.nextToken()); // 테스트케이스 갯수
+        int n = Integer.parseInt(st.nextToken()); // 나무의 갯수
+        int m = Integer.parseInt(st.nextToken()); // 최소 나무 길이
+        Long[] arr = new Long[n];
 
         // 3. 알고리즘 로직 작성
-        /** think : 그래프 저장 - 2차원 배열로 저장
-         * isVisit으로 방문 여부 확인 false면 +1
-         * 연결 방문지 큐에 넣으며 방문
-          */
+        /** think : 나무 길이가 20억까지 있으므로 일일히 비교는 불가
+         * -> 이분탐색으로 범위를 좁히자 
+         * 이분 탐색의 종료 조건 : 목표 값과 동일하거나 low == mid || high == mid
+         */
         
-        for(;t>0;t--){
-            
-            // 4. 그래프 저장
-            st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken()); //열의 갯수
-            int n = Integer.parseInt(st.nextToken()); //행의 갯수
-            int k = Integer.parseInt(st.nextToken()); //배추 위치
-            int x,y  = 0;
-            
-            int[][] arr = new int[n][m];
-            boolean[][] isVisit = new boolean[n][m]; //방문 여부 
-            int compo = 0;
-            Queue<int[]> queue = new LinkedList<>();
+        st = new StringTokenizer(br.readLine());
+        Long low = Long.parseLong(st.nextToken());
+        Long high = low;
+        arr[0] = low;
 
-            for (int i = 0; i < k; i++) {
-                st = new StringTokenizer(br.readLine());
-                x = Integer.parseInt(st.nextToken());
-                y = Integer.parseInt(st.nextToken());
-                arr[y][x] = 1;
-            }   
-
-            // 5. bfs
-            for (int i = 0; i < n; i++) {
-                for(int j = 0; j<m; j++){
-                    if(!isVisit[i][j] && arr[i][j]==1){
-                        compo++;
-                        queue.offer(new int[]{i,j});
-                        isVisit[i][j] = true;
-
-                        while(!queue.isEmpty()){
-                            int[] curr = queue.poll();
-                            x = curr[0];
-                            y = curr[1];
-                            System.out.println("x = " + x + " y = " + y + " 탐색 시작");
-                            //위
-                            if(x!=0) {
-                                if(arr[x-1][y] == 1 && isVisit[x-1][y] == false){
-                                    queue.offer(new int[]{x-1,y});
-                                    isVisit[x-1][y] =true;
-                                    System.out.println((x-1) + ", " + y + " 큐에 담음");
-                                }
-                            }
-                            //아래
-                            if(x!=n-1){
-                                if(arr[x+1][y] == 1 && isVisit[x+1][y] == false){
-                                    queue.offer(new int[]{x+1,y});
-                                    isVisit[x+1][y] = true;
-                                    System.out.println((x+1) + ", " + y + " 큐에 담음");                                    
-                                }
-                            }
-                            //좌
-                            if(y!=0){
-                                if(arr[x][y-1]==1 && isVisit[x][y-1]==false){
-                                    queue.offer(new int[]{x,y-1});
-                                    isVisit[x][y-1] = true;
-                                    System.out.println(x + ", " + (y-1) + " 큐에 담음");
-                                }
-                            }
-                            if(y!=m-1){
-                                if(arr[x][y+1]==1 && isVisit[x][y+1]==false){
-                                    queue.offer(new int[]{x,y+1});
-                                    isVisit[x][y+1] = true;
-                                    System.out.println(x + ", " + (y+1) + " 큐에 담음");
-                                }
-                            }
-                        } 
-                    }
-                }
-            }
-            // 6. 결과 출력
-            System.out.print(compo+"\n");
+        // 나무 길이 중 최대 최소 저장
+        for(int i = 1; i<n; i++ ){
+            Long tmp = Long.parseLong(st.nextToken());
+            arr[i] = tmp;
+            if(tmp<low) low = tmp;
+            else if (tmp>high) high = tmp;
         }
+
+        // 최적의 길이 이분 탐색
+        Long result;
+        while(true){
+            Long mid = (high + low)/2;
+            System.out.println("현재 mid = "+ mid);
+            if(low == mid || high == mid){
+                result = mid; break;
+            }
+
+            Long sum = 0L; // mid로 잘랐을 때 가져갈 수 있는 나무 길이
+            for(int i = 0; i<n; i++){
+                if(arr[i]>mid) sum += arr[i] - mid; 
+            }
+
+            System.out.println("현재 mid로 자른 나무 "+ sum);
+
+            if(sum>m){
+                low = mid + 1;
+                System.out.println("현재 low = "+ low);                
+            } //목표보다 더 많이 자른 경우 자르는 높이를 올려야 됨
+            else if(sum < m){
+                high = mid - 1;
+                System.out.println("현재 high = "+ high);
+            } //목표보다 더 적게 자른 경우 자르는 높이를 낮춰야 함
+            else {
+                result = mid; break;
+            }
+        }
+
+        System.out.println(result);
 
     }
 
