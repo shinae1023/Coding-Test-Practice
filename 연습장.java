2625246c37b4159c35f49a6cac2bee8017d822ec
황신애 <shinae1023@naver.com>
2026-01-21 20:33:43 +0900

    1927 풀이

--- 연습장.java
+++ 연습장.java
@@ -10,33 +10,16 @@ class Main {
         // 2. 필요한 변수 입력받기
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()); // 명령어의 수
-        Deque<Integer> stack = new ArrayDeque<>(); // 스택 선언
+        PriorityQueue<Long> minHeap = new PriorityQueue<>(); // 우선순위 큐 선언
         
         for(int i = 0; i<n; i++){
-            st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
-            if(command.equals("push")){
-                int num = Integer.parseInt(st.nextToken());
-                stack.push(num);
-            }
-            else if (command.equals("pop")){
-                if(!stack.isEmpty()){
-                    sb.append(stack.pop()).append("\n");
-                }
-                else sb.append("-1\n");
-            }
-            else if (command.equals("size")){
-                sb.append(stack.size()).append("\n");
-            }
-            else if(command.equals("empty")){
-                if(!stack.isEmpty()) sb.append("0\n");
-                else sb.append("1\n");
+            Long num = Long.parseLong(br.readLine());
+            if(num == 0 ){
+                if(minHeap.isEmpty()== true) sb.append("0\n");
+                else sb.append(minHeap.poll()).append("\n");
             }
             else{
-                if(!stack.isEmpty()){
-                    sb.append(stack.peek()).append("\n");
-                }
-                else sb.append("-1\n");
+                minHeap.add(num);
             }
         }
         System.out.println(sb);
