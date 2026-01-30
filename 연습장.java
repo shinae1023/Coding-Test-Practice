1b11a7c6eb11be3faee8a20aeb0c626252ec78b4
황신애 <shinae1023@naver.com>
2026-01-30 16:08:24 +0900

    1074

--- 연습장.java
+++ 연습장.java
@@ -2,62 +2,50 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    //1931 회의실 배정
+    //1074 Z
+    static int n,c,r = 0;
+    static int count = 0;
+    static int[][] arr;
     public static void main(String[] args) throws IOException {
         // 1. 입출력을 위한 BufferedReader, StringBuilder
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         // 2. 필요한 변수 입력받기
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
 
-        // 회의 배열 생성 및 시간 입력 받기
-        Meeting[] meets = new Meeting[n];
-        for(int i=0; i<n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+        n = (int) Math.pow(2, n);
 
-            meets[i] = new Meeting(start,end);
-        }
+        // 3. 2차원 배열 생성
+        arr = new int[n][n];
 
         // 4. 알고리즘 작성
         /**
-         * 그리디 알고리즘
-         * 정렬 기준
-         * 1순위 : 회의가 빨리 끝나는 순서
-         * 2순위 : 회의가 빨리 시작하는 순서
+         * 분할 정복
+         * 
          */
+        cnt(n,0,0);
 
-        //회의시간 정렬
-        Arrays.sort(meets,(o1,o2) -> {
-            if(o1.end==o2.end){
-                return o1.start-o2.start;
-            }
-            return o1.end - o2.end;
-        });
-        
-        // 회의 순회
-        int endTime = meets[0].end; //이전 회의 끝나는 시각
-        int count = 1; //총 가능 회의 수
-        for(int  i = 1; i<n; i++){
-            if(meets[i].start >= endTime){
-                endTime = meets[i].end;
-                count++;
-            }
-        }
-
-        System.out.println(count);
+        System.out.println(arr[c][r]);
     }
  
-    static class Meeting {
-        int start, end;
-
-        //생성자
-        Meeting(int start, int end) {
-            this.start = start;
-            this.end = end;
+    public static void cnt(int size, int x, int y){
+        while(size>2){
+            size = size/2;
+            cnt(size,x,y);
+            cnt(size,x,y+size);
+            cnt(size,x+size,y);
+            cnt(size,x+size,y+size);
+            return;
         }
+
+        arr[x][y] = count; count++;
+        arr[x][y+1] = count; count++;
+        arr[x+1][y] = count; count++;
+        arr[x+1][y+1] = count; count++;
     }
 }
 
