683c05af91cc2f9e6097276f8e6967913e33801e
황신애 <shinae1023@naver.com>
2026-01-30 15:23:22 +0900

    1931

--- 연습장.java
+++ 연습장.java
@@ -2,11 +2,7 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    //2630 색종이 만들기
-    static int white = 0;
-    static int blue = 0;
-    static int[][] arr;
-
+    //1931 회의실 배정
     public static void main(String[] args) throws IOException {
         // 1. 입출력을 위한 BufferedReader, StringBuilder
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,47 +11,53 @@ class Main {
         // 2. 필요한 변수 입력받기
         int n = Integer.parseInt(br.readLine());
 
-        arr = new int[n][n];
-
-        // 3. 2차원 배열 생성 및 입력 받기
+        // 회의 배열 생성 및 시간 입력 받기
+        Meeting[] meets = new Meeting[n];
         for(int i=0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            meets[i] = new Meeting(start,end);
         }
 
         // 4. 알고리즘 작성
         /**
-         * 시작 지점 맨 위 왼쪽, 끝 아래 오른쪽
-         * 색깔 같은지 체크 하다가 다르면 그 지점부터 재귀호출
-         * 사이즈는 n/2로 줄임
+         * 그리디 알고리즘
+         * 정렬 기준
+         * 1순위 : 회의가 빨리 끝나는 순서
+         * 2순위 : 회의가 빨리 시작하는 순서
          */
 
-        countColor(n,0,0);
-
-        System.out.println(white);
-        System.out.println(blue);
-    }
-
-    public static void countColor(int size, int x, int y){
-        int color = arr[x][y];
-
-        for(int i = x; i<x+size; i++){
-            for (int j = y; j<y+size; j++){
-                if(color != arr[i][j]){
-                    int half = size/2;
-                    countColor(half,x,y);
-                    countColor(half,x+half,y);
-                    countColor(half,x,y+half);
-                    countColor(half,x+half,y+half);
-                    return ;
-                }
+        //회의시간 정렬
+        Arrays.sort(meets,(o1,o2) -> {
+            if(o1.end==o2.end){
+                return o1.start-o2.start;
+            }
+            return o1.end - o2.end;
+        });
+        
+        // 회의 순회
+        int endTime = meets[0].end; //이전 회의 끝나는 시각
+        int count = 1; //총 가능 회의 수
+        for(int  i = 1; i<n; i++){
+            if(meets[i].start >= endTime){
+                endTime = meets[i].end;
+                count++;
             }
         }
 
-        if(color == 1) blue++;
-        else white++;
+        System.out.println(count);
     }
+ 
+    static class Meeting {
+        int start, end;
 
+        //생성자
+        Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
 }
+
