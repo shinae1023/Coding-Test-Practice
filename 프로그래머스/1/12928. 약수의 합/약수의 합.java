ac484c738470eaf5cc36fbc73aac35900772d660
황신애 <shinae1023@naver.com>
2026-07-01 14:28:37 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,20 @@
+import java.lang.Math;
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sqrt = (int)Math.sqrt(n);
+        
+        for (int i = 1; i <= sqrt; i++){
+            if(n%i == 0){
+                answer += i;
+                if (n/i != i){
+                    answer += n/i;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
