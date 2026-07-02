100a6eac24fd0edf8617bad0d6b843cb171db303
황신애 <shinae1023@naver.com>
2026-07-02 10:45:32 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int price) {
+        if (price < 100000){
+            return price;
+        }
+        else if (price >= 100000 && price < 300000){
+            return (int) (price * 0.95);
+        }
+        else if (price >= 300000 && price < 500000){
+            return (int) (price * 0.9);
+        }
+        else return (int) (price * 0.8);
+    }
+}
