627a2bcb5d73ca6078b874d064b09816b3ea4275
황신애 <shinae1023@naver.com>
2026-07-08 13:57:15 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,21 +1,20 @@
 class Solution {
-        public int getGcd(int a, int b){
-	    while (b > 0){
-		    int temp = a % b;
-		    a = b;
-		    b = temp;
-		    }
-	    return a;
-    }
-
-    public int getLcm(int a, int b){
-	    int gcd = getGcd(a,b);
-	    return a*b / gcd;
+    
+    //최대공약수 구하는 알고리즘
+    public int getGCD(int a, int b){
+        while(b>0){
+            int temp = a%b; //나머지 저장
+            a = b; // a에 b를 저장
+            b = temp; // b에 나머지 저장
+        }
+        return a; // b가 0일때의 a를 return
     }
     
     public int solution(int n) {
-        int answer = getLcm(n,6) / 6;
+        int answer = 0;
+        answer = getGCD(n,6); //최대공약수
+        answer = n*6/answer; //최소공배수 (= 두수의 곱을 최대공약수로 나눈 것) -> 피자 조각수
+        answer /= 6; //피자 한 판은 6조각이므로 6으로 나누어 피자 판 수 구함
         return answer;
     }
-
 }
