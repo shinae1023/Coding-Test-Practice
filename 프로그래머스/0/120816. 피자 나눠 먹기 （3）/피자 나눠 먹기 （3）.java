ce72f2a087fec9584378810d7a636e19028c0518
황신애 <shinae1023@naver.com>
2026-07-08 21:33:52 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -2,10 +2,8 @@ class Solution {
     public int solution(int slice, int n) {
         int share = n/slice;
         int rest = n%slice;
-
-        if(rest == 0){
-	        return share;
-        }
+        
+        if(rest == 0 ) return share;
         else return share + 1;
-        }
+    }
 }
