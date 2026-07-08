ac5523c6709f0fa198e417b292a506ad359a4fcc
황신애 <shinae1023@naver.com>
2026-07-08 14:50:21 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,14 +3,11 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(true){
-            if(n < 10){
-                answer+=n;
-                break;
-            }
+        while(n>0){
             answer += n%10;
-            n = n/10;
+            n /= 10;
         }
+
         return answer;
     }
 }
