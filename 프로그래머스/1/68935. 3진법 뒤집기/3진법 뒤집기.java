95067bcabcc303a9525dfdbf27d95bdc1b16470f
황신애 <shinae1023@naver.com>
2026-07-15 15:39:23 +0900

    [level 1] Title: 3진법 뒤집기, Time: 3.46 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,22 +1,20 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        String s = "";
-        
-        //3진법 변환
-        while (n>0){
-            s += n%3;
+        String reverse = "";
+        while(n > 0){
+            reverse += n%3;
             n /= 3;
         }
-        System.out.println(s);
-
-        Long reverse = Long.parseLong(s);
+        
+        Long num = Long.parseLong(reverse);
+        
+        int answer = 0;
         int i = 0;
         
-        while(reverse > 0){
-            answer += (reverse % 10) * (int)Math.pow(3,i);
-            reverse /= 10;
+        while(num > 0){
+            answer += (num%10) * Math.pow(3,i);
             i++;
+            num /= 10;
         }
         
         return answer;
