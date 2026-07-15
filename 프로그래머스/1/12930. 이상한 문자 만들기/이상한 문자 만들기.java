c849d1c8c5c434b8e58e619d9e48af46b57dc014
황신애 <shinae1023@naver.com>
2026-07-15 15:35:21 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 6.93 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,17 +1,19 @@
 class Solution {
     public String solution(String s) {
-        int index = 0;
         String answer = "";
-        for(int i = 0;  answer.length() != s.length(); i++){
-            if(s.charAt(i) == ' '){
+        int index = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == ' ') {
                 index = i+1;
-                answer += s.charAt(i);
+                answer += ' ';
             }
-            else if((i - index) % 2 == 0){
+            else if((i-index) % 2 == 0){
                 answer += Character.toUpperCase(s.charAt(i));
             }
             else answer += Character.toLowerCase(s.charAt(i));
         }
+        
         return answer;
     }
 }
