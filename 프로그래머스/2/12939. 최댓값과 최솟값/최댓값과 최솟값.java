a741bc06f953dd0b3b56b7fc894d6ca2d2d67105
황신애 <shinae1023@naver.com>
2026-07-21 15:36:42 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 4.94 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String [] arr = s.split(" ");
+        int min = Integer.parseInt(arr[0]);
+        int max = Integer.parseInt(arr[0]);
+        
+        for(int i = 1; i < arr.length; i++){
+            int num = Integer.parseInt(arr[i]);
+            if(min > num) min = num;
+            if(max < num) max = num;
+        }
+        
+        String answer = min +" "+ max;
+        return answer;
+    }
+}
