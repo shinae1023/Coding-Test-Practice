36bb07144e46cfb40a1ffb0c15b946d75d68c902
황신애 <shinae1023@naver.com>
2026-07-30 00:28:06 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.42 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,24 +3,22 @@ class Solution {
         String answer = "";
         StringBuilder sb = new StringBuilder(answer);
         
-        for(int i = 1; i < food.length; i++){
-            int index = sb.length() / 2;
+        for(int i = 0; i < food.length; i++){
+            int index = sb.length()/2;
             if(food[i] % 2 == 0){
-                for(int j = 0; j < food[i]; j++){
+                for(int j = 0; j < food[i]; j++ ){
                     sb.insert(index,i);
                 }
             }
-            else {
-                for(int j = 0; j < food[i]-1; j++){
-                    sb.insert(index,i);
+            else{
+                for(int j = 0; j < food[i] - 1; j++){
+                    sb.insert(index, i);
                 }
             }
         }
         
         sb.insert(sb.length()/2,0);
-        
         answer = sb.toString();
-        
         return answer;
     }
 }
