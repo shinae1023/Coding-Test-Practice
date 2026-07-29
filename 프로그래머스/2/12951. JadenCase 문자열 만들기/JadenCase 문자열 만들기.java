9549dc5105a908362db46e8f31b0fc0440358454
황신애 <shinae1023@naver.com>
2026-07-30 00:28:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 4.54 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -3,25 +3,28 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
-        Boolean isFirst = true;
+        boolean isFirst = true;
         
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
             
             if(c == ' '){
-                answer += c;
                 isFirst = true;
+                answer += c;
+                continue;
+            }
+            
+            if(isFirst){
+                c = Character.toUpperCase(c);
+                isFirst = false;
             }
             else{
-                if(isFirst){
-                    c = Character.toUpperCase(c);
-                    isFirst = false;
-                }
-                else c = Character.toLowerCase(c);
-                answer += c;
+                c = Character.toLowerCase(c);
             }
+            
+            answer += c;
         }
+        
         return answer;
     }
 }
