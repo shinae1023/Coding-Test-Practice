58565d0697ba5e7f42b8f706f7caeb47f8271f49
황신애 <shinae1023@naver.com>
2026-07-30 00:28:26 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 9.65 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -3,18 +3,22 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int zero = 0; int count = 0;
+        StringBuilder sb = new StringBuilder(s);
         
         while(!(s.equals("1"))){
             for(int i = 0; i < s.length(); i++){
-                if(s.charAt(i) == '0') zero++;
+                if(s.charAt(i) == '0'){
+                    zero++;
+                }
             }
             s = s.replace("0","");
+            
             int size = s.length();
             s = Integer.toBinaryString(size);
             count++;
         }
         
-        int[] answer = {count , zero};
+        int[] answer = {count,zero};
         return answer;
     }
 }
