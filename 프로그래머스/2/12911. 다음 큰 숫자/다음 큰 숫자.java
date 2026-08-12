c6feb1c6369787e19db18d1cad90c49ce9886f82
황신애 <shinae1023@naver.com>
2026-08-12 22:18:51 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,10 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
         int cnt = Integer.bitCount(n);
+        
         while(true){
             n++;
-            if(cnt == Integer.bitCount(n)) break;
+            if(cnt == Integer.bitCount(n)){
+                return n;
+            }
         }
-        return n;
     }
 }
