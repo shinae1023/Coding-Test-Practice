e7add25128452523ddfc79f670d5b47c05de6b47
황신애 <shinae1023@naver.com>
2026-08-12 12:36:35 +0900

    [level 2] Title: 짝지어 제거하기, Time: 27.12 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s){
+        Deque<Character> q = new ArrayDeque<>();
+        q.push(s.charAt(0));
+        
+        for(int i = 1; i < s.length(); i++){
+            if(!q.isEmpty() && q.peek() == s.charAt(i)){
+                q.pop();
+            }
+            else q.push(s.charAt(i));
+        }
+        
+        if(q.isEmpty()) return 1;
+        else return 0;
+    
+    }
+}
