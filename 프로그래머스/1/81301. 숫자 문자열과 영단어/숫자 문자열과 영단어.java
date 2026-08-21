9396f3ca13a185a27057cbb9f879c7c2e38cecff
황신애 <shinae1023@naver.com>
2026-08-21 22:15:47 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,67 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        String temp = "";
-        int i = 0;
+        String[] words = {
+            "zero", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine"
+        };
         
-        while(i < s.length()){
-            if(Character.isDigit(s.charAt(i))){
-                temp += s.charAt(i);
-                i++;
-            }
-            else{
-                String twoLetter = s.substring(i,i+2);
-                switch(twoLetter){
-                    case "ze" :
-                        temp+=0;
-                        i += 4;
-                        break;
-                    case "on" :
-                        temp += 1;
-                        i += 3;
-                        break;
-                    case "tw" :
-                        temp += 2;
-                        i += 3;
-                        break;
-                        
-                    case "th" :
-                        temp += 3;
-                        i+=5;
-                        break;
-                    case "fo" :
-                        temp += 4;
-                        i+=4;
-                        break;
-                    case "fi" :
-                        temp += 5;
-                        i+=4;
-                        break;
-                    case "si" :
-                        temp += 6;
-                        i+=3;
-                        break;
-                    case "se" :
-                        temp += 7;
-                        i+=5;
-                        break;
-                    case "ei" :
-                        temp += 8;
-                        i+= 5;
-                        break;
-                    case "ni" :
-                        temp += 9;
-                        i+=4;
-                        break;
-                    default :
-                        temp += s.charAt(i);
-                        i++;
-                }
-            }
+        for(int i = 0; i < 10; i++){
+            s = s.replace(words[i], String.valueOf(i));
         }
         
-        answer = Integer.parseInt(temp);
+        int answer = Integer.parseInt(s);
         return answer;
     }
 }
