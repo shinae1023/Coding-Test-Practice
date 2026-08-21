8044811c0595b95a0acded342c2f586ea1cbb31f
황신애 <shinae1023@naver.com>
2026-08-21 22:15:58 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,13 @@
-import java.util.*;
-
-// 8 * 6 (갈 24/ 노 24) - 둘레가 28 (14) 경우의 수 11/3 (노란색의 수가 가로 -2 * 세로 -2) = 9 , (10,4) 8*2 = 16 , (9,5) 7*3 = 21, (8,6) = 6*4 =24 
-//(갈색 + 4) /2 로 둘레를 구하고 3이상부터 늘이고 줄이고 해서 노란색이랑 맞는지 확인
-
 class Solution {
     public int[] solution(int brown, int yellow) {
         int w,h;
-        int circum = (brown + 4) / 2;
-        w = circum - 3; h = 3;
+        int a = (brown + 4) / 2;
+        w = a - 3; h = 3;
         
         while(true){
-            int y = (w-2) * (h-2);
-            if(y == yellow) break;
+            if(yellow == (w-2) * (h-2)){
+                break;
+            }
             w--; h++;
         }
         
